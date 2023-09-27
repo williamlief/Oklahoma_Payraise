d0192214fce07118aea70687549ad112ecbbf45d
Lief <williamlief@gmail.com>
2023-09-26 21:40:39 -0700

    updated with 2022 data, had to rewrite bc download is broken

--- R/0_data-cleaning/02_bls_data.R
+++ R/0_data-cleaning/02_bls_data.R
@@ -1,42 +1,45 @@
-library(gdata)
+# library(gdata)
 # note that readxl does not yet support reading from a URL: https://github.com/tidyverse/readxl/issues/278
+library(readxl)
 library(tidyverse)
 
 # Get Data ---------------------------------------------------------------------
 
-if (!file.exists("data-raw/BLS_County_Unemployment.rds")) {
+# Direct download from the BLS website: https://www.bls.gov/lau/#cntyaa
+# Grabbing County level unemployment rates from 2006 - 2020
+
+# years <- 6:22
+# 
+# URLs <- paste0("https://www.bls.gov/lau/laucnty", 
+#                stringr::str_pad(years, 2, "left", 0), 
+#                ".xlsx")
+# 
+
+# NOTE: in Sept 2023 code above was broken on windows with R 4.3.1, instead of
+# fixing I manually downloaded bls data from https://www.bls.gov/lau/tables.htm#mcounty
+# for 2006 to 2022 to raw-data/BLS. This data is available in the OSF repository.
+
+files <- list.files("data-raw/BLS/", full.names = TRUE)
+
+raw  <- files %>% 
+  map_dfr(function(x) read_xlsx(x, skip = 4, 
+                                col_types = "text")) 
+
+saveRDS(raw, "data-raw/BLS/BLS_County_Unemployment.rds")
   
-  # Direct download from the BLS website: https://www.bls.gov/lau/#cntyaa
-  # Grabbing County level unemployment rates from 2006 - 2020
-  
-  years <- 6:20
-  
-  URLs <- paste0("http://www.bls.gov/lau/laucnty", 
-                 stringr::str_pad(years, 2, "left", 0), 
-                 ".xlsx")
-  
-  raw  <- URLs %>% map_dfr(function(x) read.xls(x, skip = 3, colClasses = "character"))
-  saveRDS(raw, "data-raw/BLS_County_Unemployment.rds")
-  
-} else { 
-  
-  print("Raw data file has already been created, loading downloaded file")
-  raw <- readRDS("data-raw/BLS_County_Unemployment.RDS")
-  
-}
 
 # Clean Data -------------------------------------------------------------------
 
 df <- raw %>% 
-  rename(laus_code = Code, 
-         state_code = Code.1, 
-         county_code = Code.2,
-         county_name = County.Name.State.Abbreviation, 
+  rename(laus_code = Code...1, 
+         state_code = Code...2, 
+         county_code = Code...3,
+         county_name = `County Name/State Abbreviation`, 
          year = Year, 
          labor_force = Force,
          employed = Employed,
          unemployed = Unemployed,
-         unemployment_rate = X...) %>% 
+         unemployment_rate = `(%)`) %>% 
   filter(
     state_code != "", # 2 footer rows per year
     #state_code %in% c("29", "48") # TX and OK - actually leave in all states
@@ -49,6 +52,6 @@ df <- raw %>%
                 as.numeric)
          ) %>%  
   # remove commas and convert to double
-  select(-X)
+  select(-...6)
 
 saveRDS(df, "Data/BLS_County_Unemployment.rds")
